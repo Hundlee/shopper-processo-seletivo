738b4baf0a8dcb54ee06d4f709c2659da4201377
Gabriel Mesquita <Mesquitag884@gmail.com>
2024-08-30 11:45:34 -0300

    feat: add new routes and controllers

--- src/app.ts
+++ src/app.ts
@@ -1,17 +1,26 @@
-import express, { Request, Response } from "express";
+import express from "express";
 import dotenv from "dotenv";
+import {
+    confirmMeasure,
+    createMeasure,
+    listMeasures,
+} from "./controllers/measureController";
 
 dotenv.config();
 
 const app = express();
-const PORT = process.env.PORT;
+const PORT = 8081;
 
 app.use(express.json());
+
+app.post("/upload", createMeasure);
+app.patch("/confirm", confirmMeasure);
+app.get("/:customer_code/list", listMeasures);
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
 
-app.get("/", (req: Request, res: Response) => {
+app.get("/", (req, res) => {
     res.send("Hello World!");
 });
